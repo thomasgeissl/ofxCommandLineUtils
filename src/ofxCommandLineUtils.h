896110a1eb76c0c6e62b15a7e74de8bb087026a7
Thomas Geissl <thomas.geissl@gmail.com>
2020-03-20 15:00:31 +0100

    added class and made functions static

--- src/ofxCommandLineUtils.h
+++ src/ofxCommandLineUtils.h
@@ -1,82 +1,86 @@
 #pragma once
-#include "./rang/include/rang.hpp"
+#include "./rang.hpp"
 #include "./cxxopts.hpp"
 
-bool getBoolAnswer(std::string question, bool defaultAnswer = true)
+class ofxCommandLineUtils
 {
-    std::cin.clear();
-    std::string defaultAnswerAsString = defaultAnswer ? "y" : "n";
-    std::cout << rang::style::bold << question<< rang::style::reset;
-    std::cout << " [y|n] ("<< rang::style::bold << rang::fg::green << defaultAnswerAsString << rang::style::reset << rang::fg::reset << ") ";
-    std::string answer;
-    getline(cin, answer);
-    if(answer == "y")
+public:
+    static bool getBoolAnswer(std::string question, bool defaultAnswer = true)
     {
-        return true;
+        std::cin.clear();
+        std::string defaultAnswerAsString = defaultAnswer ? "y" : "n";
+        std::cout << rang::style::bold << question << rang::style::reset;
+        std::cout << " [y|n] (" << rang::style::bold << rang::fg::green << defaultAnswerAsString << rang::style::reset << rang::fg::reset << ") ";
+        std::string answer;
+        getline(cin, answer);
+        if (answer == "y")
+        {
+            return true;
+        }
+        else if (answer == "n")
+        {
+            return false;
+        }
+        else if (answer.empty())
+        {
+            return defaultAnswer;
+        }
+        else
+        {
+            ofLogError("") << "invalid answer!";
+            return getBoolAnswer(question);
+        }
     }
-    else if(answer == "n")
-    {
-        return false;
-    }
-    else if(answer.empty())
-    {
-        return defaultAnswer;
-    }
-    else
-    {
-        ofLogError("")<<"invalid answer!";
-        return getBoolAnswer(question);
-    }
-}
 
-std::string getStringAnswer(std::string question, std::string defaultAnswer = "")
-{
-    std::cin.clear();
-    std::string answer;
-    std::cout << rang::style::bold << question<< rang::style::reset;
-    std::cout << " ("<< rang::style::bold << rang::fg::green << defaultAnswer << rang::style::reset << rang::fg::reset << ") ";
-    getline(cin, answer);
-    if(answer.empty())
+    static std::string getStringAnswer(std::string question, std::string defaultAnswer = "")
     {
-        return defaultAnswer;
+        std::cin.clear();
+        std::string answer;
+        std::cout << rang::style::bold << question << rang::style::reset;
+        std::cout << " (" << rang::style::bold << rang::fg::green << defaultAnswer << rang::style::reset << rang::fg::reset << ") ";
+        getline(cin, answer);
+        if (answer.empty())
+        {
+            return defaultAnswer;
+        }
+        return answer;
     }
-    return answer;
-}
 
-std::string getOptionAnswer(std::string question, std::vector<std::string> options)
-{
-    std::cin.clear();
-    std::string answer;
-    std::cout << rang::style::bold << question<< rang::style::reset << endl;
-    for(int i = 0; i < options.size(); i++)
+    static std::string getOptionAnswer(std::string question, std::vector<std::string> options)
     {
-        std::cout<<"("<<i<<") "<<options[i]<<std::endl;
+        std::cin.clear();
+        std::string answer;
+        std::cout << rang::style::bold << question << rang::style::reset << endl;
+        for (int i = 0; i < options.size(); i++)
+        {
+            std::cout << "(" << i << ") " << options[i] << std::endl;
+        }
+        std::cout << std::endl;
+        getline(cin, answer);
+        int optionIndex = ofToInt(answer);
+        if (optionIndex < options.size())
+        {
+            return options[optionIndex];
+        }
+        else
+        {
+            ofLogError("") << "invalid index";
+            return getOptionAnswer(question, options);
+        }
     }
-    std::cout<<std::endl;
-    getline(cin, answer);
-    int optionIndex = ofToInt(answer);
-    if(optionIndex < options.size())
+    static int getIntAnswer(std::string question, int defaultAnswer = 0)
     {
-        return options[optionIndex];
-    }
-    else
-    {
-        ofLogError("")<<"invalid index";
-        return getOptionAnswer(question, options);
-    }
-}
+        std::cin.clear();
+        std::string answer;
 
-int getIntAnswer(std::string question, int defaultAnswer = 0){
-    std::cin.clear();
-    std::string answer;
+        std::cout << rang::style::bold << question << rang::style::reset;
+        std::cout << " (" << rang::style::bold << rang::fg::green << defaultAnswer << rang::style::reset << rang::fg::reset << ") ";
 
-    std::cout << rang::style::bold << question<< rang::style::reset;
-    std::cout << " ("<< rang::style::bold << rang::fg::green << defaultAnswer << rang::style::reset << rang::fg::reset << ") ";
-
-    getline(cin, answer);
-    if(answer.empty())
-    {
-        return defaultAnswer;
+        getline(cin, answer);
+        if (answer.empty())
+        {
+            return defaultAnswer;
+        }
+        return ofToInt(answer);
     }
-    return ofToInt(answer);
 }
